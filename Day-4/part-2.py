3dc41e920ad20288da6572014102082a05845414
ooliver1 <oliverwilkes2006@icloud.com>
2021-12-07 19:28:28 +0000

    feat(4): part 2

---
+++ Day-4/part-2.py
@@ -0,0 +1,37 @@
+from pprint import pformat
+
+with open("input.txt") as f:
+    data = f.readlines()
+
+drawn_numbers = data[0].split(",")
+boards = [
+    [[int(x) for x in line.split()] for line in data[i + 1 : i + 6]]
+    for i in range(1, len(data), 6)
+]
+
+marked = [[[False for _ in range(5)] for _ in range(5)] for _ in range(len(boards))]
+won = [False for _ in range(len(boards))]
+for number in drawn_numbers:
+    for i, board in enumerate(boards):
+        for row in range(5):
+            for col in range(5):
+                if boards[i][row][col] == int(number):
+                    marked[i][row][col] = True
+        if any(all(marked[i][row][col] for col in range(5)) for row in range(5)) or any(
+            all(marked[i][col][row] for col in range(5)) for row in range(5)
+        ):
+            won[i] = True
+            if all(won):
+                print(f"{number} wins")
+                print(pformat(board))
+                print(pformat(marked[i]))
+                total = sum(
+                    sum(
+                        boards[i][row][col]
+                        for col in range(5)
+                        if not marked[i][row][col]
+                    )
+                    for row in range(5)
+                )
+                print(f"score: {total * int(number)}")
+                exit()
